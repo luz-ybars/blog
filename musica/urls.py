138bf482c101279e186d7ef0b5a0d7718c3f305e
luihs126 <luihs126@gmail.com>
2025-08-07 20:50:58 -0300

    vista de inicio

---
+++ musica/urls.py
@@ -0,0 +1,7 @@
+# musica/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+]
--- myblog/settings.py
+++ myblog/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'eventos',
     'noticias',
     'auth_app'
+    
 ]
 
 
@@ -126,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static')),
+STATICFILES_DIRS = ('/ruta/static',)  
+
 
 MEDIA = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
